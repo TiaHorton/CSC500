792784ac0ee7325f3ee893064e21cd0cf807b285
Tia Horton <tia.horton@csuglobal.edu>
2025-07-23 23:03:42 -0400

    Assignment 1_P2

---
+++ Assignment_1_P2.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the first number: ")) 
+num2 = int(input("Enter the second number: ")) 
+multiply = num1 * num2
+division = num1 / num2 
+print(f"Multiply result:, {multiply}")
+print(f"Division result:, {division}")
